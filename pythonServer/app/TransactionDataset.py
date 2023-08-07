7cd7a636e2793470ebb83693e9397ad3e7845f07
Wentao Mao <wentaom@foxmail.com>
2023-08-07 10:15:22 +0100

    moved transaction_cleanedtest file to the server folder created routers for TransactionDataset and kmeans

--- pythonServer/app/TransactionDataset.py
+++ pythonServer/app/TransactionDataset.py
@@ -4,33 +4,150 @@ from sklearn.cluster import KMeans
 
 
 class TransactionDataset:
+    '''
+        a dataset with the following columns: transactionNumber (str), transactionDate (datetime), 
+        transactionType (str), transactionDescription(str), balance (float), category (str), locationCity (str), locationCountry (str), 
+        isCredit (boolean), transactionAmount (float)
+        replace all null value in str columns with '' // todo
+    '''
+
     def __init__(self, csvPath: str):
         '''
-            read transactions from csv file, the columns name will be convert to cammelCase
+            read transactions from csv file, the data will be initialised
+        '''
+
+        self.dataframe: pd.DataFrame = pd.read_csv(csvPath)
+        self.__convertColumnNameToCammelCase()
+        self.__addTransactionAmountInfo()
+        self.__cleanDateInfo()
+        self.dataframe.set_index('transactionNumber')
+
+    def getDataframe(self) -> pd.DataFrame:
+        '''
+            return the internal dataframe for the transaction with the following columns: transactionNumber (str), transactionDate (datetime), 
+            transactionType (str), transactionDescription(str), balance (float), category (str), locationCity (str), locationCountry (str), 
+            isCredit (boolean), transactionAmount (float)
+        '''
+        return self.dataframe
+
+    def __convertColumnNameToCammelCase(self) -> bool:
+        '''
+            convert column name of self.dataframe to cammelCase inplace
+            return true if successed, false if failed
         '''
         try:
-            self.dataframe: pd.DataFrame = pd.read_csv(csvPath)
-            self.dataframe = self._convertColumnNameToCammelCase(inplace=False)
-            assert(self.dataframe!=None, 'dataframe become none')
+            columnNameMapping = dict()
+            for initColumnName in self.dataframe.columns:
+                initColumnName: str
+                if len(initColumnName.split(' ')) == 2:
+                    firstPart = initColumnName.split(' ')[0].lower()
+                    secondPart = initColumnName.split(
+                        ' ')[1].lower().capitalize()
+                    columnNameMapping[initColumnName] = firstPart + secondPart
+                else:
+                    columnNameMapping[initColumnName] = initColumnName.lower()
+            self.dataframe.rename(columns=columnNameMapping, inplace=True)
+            return True
         except:
-            print('fail to read file from: ', csvPath)
+            return False
 
-    def _convertColumnNameToCammelCase(self, inplace: bool):
+    def __addTransactionAmountInfo(self):
         '''
-            a private method
-            convert column name to cammelCase
-            Return None if inplace==True
-            otherviseDataFrame with column names in cammelCase
+            inplace:
+            add the isCredit, if the creditAmount in the dataframe is na, set false, else true
+            a transaction can be either be a credit transaction or a debit transaction
+            add transaction Amount based on isCredit, if isCredit is true, use Credit Amount, else use Debit Amount
+            return true if successed, false if failed
         '''
-        columnNameMapping = dict()
-        for initColumnName in self.dataframe.columns:
-            initColumnName: str
-            if len(initColumnName.split(' ')) == 2:
-                firstPart = initColumnName.split(' ')[0].lower()
-                secondPart = initColumnName.split(' ')[1].lower().capitalize()
-                columnNameMapping[initColumnName] = firstPart + secondPart
-            else:
-                columnNameMapping[initColumnName] = initColumnName.lower()
-        newDfOrNone = self.dataframe.rename(
-            columns=columnNameMapping, inplace=inplace)
-        return newDfOrNone
+        try:
+            # add isCredit: reference: https://stackoverflow.com/questions/71000585/create-a-new-column-in-pandas-dataframe-based-on-the-nan-values-in-another-col
+            self.dataframe['isCredit'] = self.dataframe['creditAmount'].isna(
+            ) == False
+            # add transaction Amount based on isCredit, if isCredit is true, use Credit Amount, else use Debit Amount
+            self.dataframe['transactionAmount'] = self.dataframe.apply(
+                lambda row: row.creditAmount if row['isCredit'] else row.debitAmount, axis=1)
+            return True
+        except:
+            return False
+
+    def __cleanDateInfo(self):
+        '''
+            inplace:
+            set the transactionData column to be datetime object
+            add day of year column 1 to 366
+            add day of week: 1to7 1: monday, 2: tuesday...
+            week of the year 1 to 53
+            return true if successed, false if failed
+        '''
+        try:
+            self.dataframe['transactionDate'] = pd.to_datetime(
+                self.dataframe['transactionDate'], dayfirst=True)
+            self.dataframe['dayOfYear'] = self.dataframe['transactionDate'].dt.dayofyear
+            self.dataframe['dayOfWeek'] = self.dataframe['transactionDate'].dt.dayofweek + 1
+            self.dataframe['weekOfYear'] = self.dataframe['transactionDate'].dt.isocalendar(
+            ).week
+            return True
+        except:
+            return False
+
+    def __getNumericalDataFromCategoricalData(self, categoricalColumnName) -> pd.Series:
+        '''
+            assume categoricalColumnName exists
+            takes a name of a column with categorical data (str, datetime, boolean), and return a series with the numerical data.
+        '''
+        numericalColumn: pd.Series = self.dataframe[categoricalColumnName].replace(set(
+            self.dataframe[categoricalColumnName]), [i for i in range(len(set(self.dataframe[categoricalColumnName])))])
+        return numericalColumn
+
+    def getColumn(self, columnName, toNumerical=False) -> list:
+        '''
+            assume the columnName exist
+            if the column is categorical and toNumerical==True, return a list of number represents the category of the columnName
+            otherwise return a list of value of the columnName
+        '''
+        assert columnName in self.dataframe.columns, f"{columnName} doesn't exist"
+        if toNumerical:
+            return self.__getNumericalDataFromCategoricalData(columnName)
+        else:
+            return self.dataframe[columnName]
+
+    def clusterByKMeans(self, metric1, metric2, numberOfCluster):
+        '''
+            assume metric1 and metric2 exist in the column names.
+            run KMean clustering algorithm based on the two metrics
+            if any metric is not int or float, they will be try to convert to float.
+            set the clusterId column to be the result of clustering algorithm
+        '''
+        assert metric1 in self.dataframe.columns, f"{metric1} does not exist"
+        assert metric2 in self.dataframe.columns, f"{metric2} does not exist"
+
+        # get the numerical value of two columns
+        x1 = self.getColumn(metric1, toNumerical=True).to_numpy()
+        x2 = self.getColumn(metric2, toNumerical=True).to_numpy()
+        X = np.dstack((x1, x2))[0]
+        # run kmean
+        # reference: https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+        kmeans = KMeans(n_clusters=numberOfCluster, random_state=0).fit(X)
+        # update the clusterId column
+        self.dataframe['cluster'] = kmeans.labels_
+        return True
+
+    def getClusterIdOfTransactionNumber(self) -> dict:
+        '''
+            if the cluster algorithm has runned return a dictionary where the key is transactionNumber and the value is the cluster.
+        '''
+        return self.getDataframe()[['transactionNumber', 'cluster']].set_index('transactionNumber').to_dict(orient="index")
+
+    def isValidColumnName(self, columnNameToCheck: str) -> bool:
+        '''
+            columnNameToCheck: the column name to check
+            return True if the columnName is valid
+            return False if not
+        '''
+        return columnNameToCheck in self.getColumnNames()
+    
+    def getColumnNames(self)->list:
+        '''
+            Return a list of valid columnNames
+        '''
+        return list(self.getDataframe().columns)
--- pythonServer/app/main.py
+++ pythonServer/app/main.py
@@ -1,14 +1,38 @@
 from typing import Union
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+from app.TransactionDataset import TransactionDataset
+import json
+import os
+# start server app
 app = FastAPI()
 
+# initialise the dataset
+try:
+    transactionDataset = TransactionDataset(os.getcwd()+'''/data/transaction_cleanedtest.csv''')
+    print(transactionDataset.getDataframe())
+except Exception as error:
+    print(error)
 
+# testing
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# get the transaction
+@app.get("/transactionData")
+def getTransactionData():
+    return json.loads(transactionDataset.getDataframe().to_json(orient='records'))
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# get cluster id by transactionNumber
+@app.get("/transactionData/kmean")
+def getClusterId(metric1: str, metric2: str, numberOfCluster: int):
+    if transactionDataset.isValidColumnName(metric1) == False:
+        # reference for error http handling: https://fastapi.tiangolo.com/tutorial/handling-errors/#:~:text=When%20a%20request%20contains%20invalid,to%20decorate%20the%20exception%20handler.
+        raise HTTPException(status_code=404, detail=f"{metric1} is invalid, only support {str(transactionDataset.getColumnNames())}")
+    elif transactionDataset.isValidColumnName(metric2) == False:
+        raise HTTPException(status_code=404, detail=f"{metric2} is invalid, only support {str(transactionDataset.getColumnNames())}")
+    else:
+        # run kmean clustering algorithm
+        transactionDataset.clusterByKMeans(metric1, metric2, numberOfCluster)
+        # return the transactionNumber:clusterId pair
+        return transactionDataset.getClusterIdOfTransactionNumber()
