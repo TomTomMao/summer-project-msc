9a52e7cc3769b17675aa21b184432a771434ec88
Wentao Mao <wentao.mao@outlook.com>
2025-03-15 16:21:05 +0800

    hide year input when superpositioned mode is on

--- frontend/app/dashboard/components/CalendarView3/CalendarView3.tsx
+++ frontend/app/dashboard/components/CalendarView3/CalendarView3.tsx
@@ -615,13 +615,13 @@ export function CalendarViewYearController() {
         }}>
             <input type="checkbox" name="" id="isSuperPositioned" value='isSuperPositioned' checked={isSuperPositioned} onChange={handleChangeIsSuperpositioned} />
             <label htmlFor="isSuperPositioned" style={{ marginRight: '2px' }}>Super Positioned</label>
-            <input style={{ width: '60px', height: '100%', border: isSuperPositioned ? '1px gray solid' : '1px black solid', color: isSuperPositioned ? 'gray' : 'black' }}
+            {!isSuperPositioned && <input style={{ width: '60px', height: '100%', border: isSuperPositioned ? '1px gray solid' : '1px black solid', color: isSuperPositioned ? 'gray' : 'black' }}
                 disabled={isSuperPositioned}
                 type="number"
                 name=""
                 id=""
                 value={currentYear}
-                onChange={e => handleChangeCurrentYear(parseInt(e.target.value))} />
+                onChange={e => handleChangeCurrentYear(parseInt(e.target.value))} />}
         </div>
     )
 }
